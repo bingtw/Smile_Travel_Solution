6abfbe83cabb9bc93945a596e81532a774696715
Bing Hsueh <bing.tangbing@gmail.com>
2016-01-02 00:49:00 +0800

    initial release, 1.0.0.0

--- Travel_Agent/Form_TravelAgent.Designer.cs
+++ Travel_Agent/Form_TravelAgent.Designer.cs
@@ -33,16 +33,22 @@
             this.tsmi_Config_DB = new System.Windows.Forms.ToolStripMenuItem();
             this.tabControl_Main = new System.Windows.Forms.TabControl();
             this.tabPage_Selector = new System.Windows.Forms.TabPage();
+            this.button_ExportResult = new System.Windows.Forms.Button();
+            this.button_AddSelection = new System.Windows.Forms.Button();
+            this.dataGridView_Result = new System.Windows.Forms.DataGridView();
+            this.dataGridView_TravelDataList = new System.Windows.Forms.DataGridView();
+            this.label_SelectTable = new System.Windows.Forms.Label();
+            this.button_RefreshTabList = new System.Windows.Forms.Button();
+            this.label_TableChoice = new System.Windows.Forms.Label();
+            this.comboBox_TableList = new System.Windows.Forms.ComboBox();
             this.statusStrip_Main = new System.Windows.Forms.StatusStrip();
             this.toolStripStatusLabel_DB = new System.Windows.Forms.ToolStripStatusLabel();
             this.toolStripStatusLabel_DB_File = new System.Windows.Forms.ToolStripStatusLabel();
-            this.tabPage2 = new System.Windows.Forms.TabPage();
-            this.comboBox_TableList = new System.Windows.Forms.ComboBox();
-            this.label_TableChoice = new System.Windows.Forms.Label();
             this.menuStrip_Main.SuspendLayout();
             this.tabControl_Main.SuspendLayout();
             this.tabPage_Selector.SuspendLayout();
-            this.statusStrip_Main.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Result)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_TravelDataList)).BeginInit();
             this.SuspendLayout();
             // 
             // menuStrip_Main
@@ -51,7 +57,7 @@
             this.設定ToolStripMenuItem});
             this.menuStrip_Main.Location = new System.Drawing.Point(0, 0);
             this.menuStrip_Main.Name = "menuStrip_Main";
-            this.menuStrip_Main.Size = new System.Drawing.Size(784, 24);
+            this.menuStrip_Main.Size = new System.Drawing.Size(1008, 24);
             this.menuStrip_Main.TabIndex = 0;
             this.menuStrip_Main.Text = "menuStrip_Main";
             // 
@@ -60,50 +66,141 @@
             this.設定ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.tsmi_Config_DB});
             this.設定ToolStripMenuItem.Name = "設定ToolStripMenuItem";
-            this.設定ToolStripMenuItem.Size = new System.Drawing.Size(45, 20);
+            this.設定ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
             this.設定ToolStripMenuItem.Text = "設定";
             // 
             // tsmi_Config_DB
             // 
             this.tsmi_Config_DB.Name = "tsmi_Config_DB";
-            this.tsmi_Config_DB.Size = new System.Drawing.Size(139, 22);
+            this.tsmi_Config_DB.Size = new System.Drawing.Size(134, 22);
             this.tsmi_Config_DB.Text = "資料庫來源";
             this.tsmi_Config_DB.Click += new System.EventHandler(this.tsmi_Config_DB_Click);
             // 
             // tabControl_Main
             // 
             this.tabControl_Main.Controls.Add(this.tabPage_Selector);
-            this.tabControl_Main.Controls.Add(this.tabPage2);
             this.tabControl_Main.Dock = System.Windows.Forms.DockStyle.Fill;
             this.tabControl_Main.Location = new System.Drawing.Point(0, 24);
             this.tabControl_Main.Name = "tabControl_Main";
             this.tabControl_Main.SelectedIndex = 0;
-            this.tabControl_Main.Size = new System.Drawing.Size(784, 537);
+            this.tabControl_Main.Size = new System.Drawing.Size(1008, 660);
             this.tabControl_Main.TabIndex = 1;
             // 
             // tabPage_Selector
             // 
+            this.tabPage_Selector.Controls.Add(this.button_ExportResult);
+            this.tabPage_Selector.Controls.Add(this.button_AddSelection);
+            this.tabPage_Selector.Controls.Add(this.dataGridView_Result);
+            this.tabPage_Selector.Controls.Add(this.dataGridView_TravelDataList);
+            this.tabPage_Selector.Controls.Add(this.label_SelectTable);
+            this.tabPage_Selector.Controls.Add(this.button_RefreshTabList);
             this.tabPage_Selector.Controls.Add(this.label_TableChoice);
             this.tabPage_Selector.Controls.Add(this.comboBox_TableList);
             this.tabPage_Selector.Controls.Add(this.statusStrip_Main);
             this.tabPage_Selector.Location = new System.Drawing.Point(4, 22);
             this.tabPage_Selector.Name = "tabPage_Selector";
             this.tabPage_Selector.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage_Selector.Size = new System.Drawing.Size(776, 511);
+            this.tabPage_Selector.Size = new System.Drawing.Size(1000, 634);
             this.tabPage_Selector.TabIndex = 0;
             this.tabPage_Selector.Text = "菜單組合";
             this.tabPage_Selector.UseVisualStyleBackColor = true;
             // 
+            // button_ExportResult
+            // 
+            this.button_ExportResult.Location = new System.Drawing.Point(823, 6);
+            this.button_ExportResult.Name = "button_ExportResult";
+            this.button_ExportResult.Size = new System.Drawing.Size(168, 30);
+            this.button_ExportResult.TabIndex = 8;
+            this.button_ExportResult.Text = "儲存結果";
+            this.button_ExportResult.UseVisualStyleBackColor = true;
+            this.button_ExportResult.Click += new System.EventHandler(this.button_ExportResult_Click);
+            // 
+            // button_AddSelection
+            // 
+            this.button_AddSelection.Location = new System.Drawing.Point(455, 41);
+            this.button_AddSelection.Name = "button_AddSelection";
+            this.button_AddSelection.Size = new System.Drawing.Size(80, 564);
+            this.button_AddSelection.TabIndex = 7;
+            this.button_AddSelection.Text = "增加至清單";
+            this.button_AddSelection.UseVisualStyleBackColor = true;
+            this.button_AddSelection.Click += new System.EventHandler(this.button_AddSelection_Click);
+            // 
+            // dataGridView_Result
+            // 
+            this.dataGridView_Result.AllowUserToAddRows = false;
+            this.dataGridView_Result.AllowUserToDeleteRows = false;
+            this.dataGridView_Result.AllowUserToOrderColumns = true;
+            this.dataGridView_Result.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_Result.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView_Result.Location = new System.Drawing.Point(541, 41);
+            this.dataGridView_Result.MultiSelect = false;
+            this.dataGridView_Result.Name = "dataGridView_Result";
+            this.dataGridView_Result.ReadOnly = true;
+            this.dataGridView_Result.RowTemplate.ReadOnly = true;
+            this.dataGridView_Result.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView_Result.Size = new System.Drawing.Size(450, 564);
+            this.dataGridView_Result.TabIndex = 6;
+            // 
+            // dataGridView_TravelDataList
+            // 
+            this.dataGridView_TravelDataList.AllowUserToAddRows = false;
+            this.dataGridView_TravelDataList.AllowUserToDeleteRows = false;
+            this.dataGridView_TravelDataList.AllowUserToOrderColumns = true;
+            this.dataGridView_TravelDataList.AllowUserToResizeRows = false;
+            this.dataGridView_TravelDataList.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView_TravelDataList.EditMode = System.Windows.Forms.DataGridViewEditMode.EditProgrammatically;
+            this.dataGridView_TravelDataList.Location = new System.Drawing.Point(9, 41);
+            this.dataGridView_TravelDataList.MultiSelect = false;
+            this.dataGridView_TravelDataList.Name = "dataGridView_TravelDataList";
+            this.dataGridView_TravelDataList.ReadOnly = true;
+            this.dataGridView_TravelDataList.RowTemplate.ReadOnly = true;
+            this.dataGridView_TravelDataList.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dataGridView_TravelDataList.Size = new System.Drawing.Size(440, 564);
+            this.dataGridView_TravelDataList.TabIndex = 5;
+            // 
+            // label_SelectTable
+            // 
+            this.label_SelectTable.AutoSize = true;
+            this.label_SelectTable.Location = new System.Drawing.Point(6, 19);
+            this.label_SelectTable.Name = "label_SelectTable";
+            this.label_SelectTable.Size = new System.Drawing.Size(77, 12);
+            this.label_SelectTable.TabIndex = 4;
+            this.label_SelectTable.Text = "選擇資料表：";
+            // 
+            // button_RefreshTabList
+            // 
+            this.button_RefreshTabList.Location = new System.Drawing.Point(305, 16);
+            this.button_RefreshTabList.Name = "button_RefreshTabList";
+            this.button_RefreshTabList.Size = new System.Drawing.Size(82, 19);
+            this.button_RefreshTabList.TabIndex = 3;
+            this.button_RefreshTabList.Text = "重新整理";
+            this.button_RefreshTabList.UseVisualStyleBackColor = true;
+            this.button_RefreshTabList.Click += new System.EventHandler(this.button_RefreshTabList_Click);
+            // 
+            // label_TableChoice
+            // 
+            this.label_TableChoice.AutoSize = true;
+            this.label_TableChoice.Location = new System.Drawing.Point(18, 19);
+            this.label_TableChoice.Name = "label_TableChoice";
+            this.label_TableChoice.Size = new System.Drawing.Size(0, 12);
+            this.label_TableChoice.TabIndex = 2;
+            // 
+            // comboBox_TableList
+            // 
+            this.comboBox_TableList.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox_TableList.FormattingEnabled = true;
+            this.comboBox_TableList.Location = new System.Drawing.Point(94, 16);
+            this.comboBox_TableList.Name = "comboBox_TableList";
+            this.comboBox_TableList.Size = new System.Drawing.Size(205, 20);
+            this.comboBox_TableList.TabIndex = 1;
+            this.comboBox_TableList.SelectedIndexChanged += new System.EventHandler(this.comboBox_TableList_SelectedIndexChanged);
+            // 
             // statusStrip_Main
             // 
-            this.statusStrip_Main.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.toolStripStatusLabel_DB,
-            this.toolStripStatusLabel_DB_File});
-            this.statusStrip_Main.Location = new System.Drawing.Point(3, 486);
+            this.statusStrip_Main.Location = new System.Drawing.Point(3, 609);
             this.statusStrip_Main.Name = "statusStrip_Main";
-            this.statusStrip_Main.Size = new System.Drawing.Size(770, 22);
+            this.statusStrip_Main.Size = new System.Drawing.Size(994, 22);
             this.statusStrip_Main.TabIndex = 0;
-            this.statusStrip_Main.Text = "statusStrip_Main";
             // 
             // toolStripStatusLabel_DB
             // 
@@ -116,38 +213,11 @@
             this.toolStripStatusLabel_DB_File.Name = "toolStripStatusLabel_DB_File";
             this.toolStripStatusLabel_DB_File.Size = new System.Drawing.Size(0, 17);
             // 
-            // tabPage2
-            // 
-            this.tabPage2.Location = new System.Drawing.Point(4, 22);
-            this.tabPage2.Name = "tabPage2";
-            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
-            this.tabPage2.Size = new System.Drawing.Size(776, 511);
-            this.tabPage2.TabIndex = 1;
-            this.tabPage2.Text = "tabPage2";
-            this.tabPage2.UseVisualStyleBackColor = true;
-            // 
-            // comboBox_TableList
-            // 
-            this.comboBox_TableList.FormattingEnabled = true;
-            this.comboBox_TableList.Location = new System.Drawing.Point(94, 18);
-            this.comboBox_TableList.Name = "comboBox_TableList";
-            this.comboBox_TableList.Size = new System.Drawing.Size(205, 21);
-            this.comboBox_TableList.TabIndex = 1;
-            // 
-            // label_TableChoice
-            // 
-            this.label_TableChoice.AutoSize = true;
-            this.label_TableChoice.Location = new System.Drawing.Point(18, 21);
-            this.label_TableChoice.Name = "label_TableChoice";
-            this.label_TableChoice.Size = new System.Drawing.Size(70, 13);
-            this.label_TableChoice.TabIndex = 2;
-            this.label_TableChoice.Text = "選擇資料表:";
-            // 
             // Form_TravelAgent
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.ClientSize = new System.Drawing.Size(1008, 684);
             this.Controls.Add(this.tabControl_Main);
             this.Controls.Add(this.menuStrip_Main);
             this.MainMenuStrip = this.menuStrip_Main;
@@ -159,8 +229,8 @@
             this.tabControl_Main.ResumeLayout(false);
             this.tabPage_Selector.ResumeLayout(false);
             this.tabPage_Selector.PerformLayout();
-            this.statusStrip_Main.ResumeLayout(false);
-            this.statusStrip_Main.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_Result)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView_TravelDataList)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -173,12 +243,17 @@
         private System.Windows.Forms.ToolStripMenuItem tsmi_Config_DB;
         private System.Windows.Forms.TabControl tabControl_Main;
         private System.Windows.Forms.TabPage tabPage_Selector;
-        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.Button button_RefreshTabList;
+        private System.Windows.Forms.Label label_TableChoice;
+        private System.Windows.Forms.ComboBox comboBox_TableList;
         private System.Windows.Forms.StatusStrip statusStrip_Main;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_DB;
         private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel_DB_File;
-        private System.Windows.Forms.Label label_TableChoice;
-        private System.Windows.Forms.ComboBox comboBox_TableList;
+        private System.Windows.Forms.DataGridView dataGridView_TravelDataList;
+        private System.Windows.Forms.Label label_SelectTable;
+        private System.Windows.Forms.DataGridView dataGridView_Result;
+        private System.Windows.Forms.Button button_AddSelection;
+        private System.Windows.Forms.Button button_ExportResult;
     }
 }
 
--- Travel_Agent/Form_TravelAgent.cs
+++ Travel_Agent/Form_TravelAgent.cs
@@ -4,19 +4,23 @@ using System.IO;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.OleDb;
 using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using ClosedXML.Excel;
 
 namespace Travel_Agent
 {
     public partial class Form_TravelAgent : Form
     {
         private const string CONFIG_FILE = "TravelAgent.config";
+        private const string CONNECTION_STRING = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=";
 
         private string _DB_File_Name = string.Empty;
+        private string _DB_Connection_String;
 
         public Form_TravelAgent()
         {
@@ -52,6 +56,9 @@ namespace Travel_Agent
                     result_saveconfig = SaveDbFileToConfigFile(CONFIG_FILE, this._DB_File_Name);
                     this._DB_File_Name = ofd_DB.FileName;
                     this.toolStripStatusLabel_DB_File.Text = this._DB_File_Name;
+
+                    // load table list from Access DB file
+                    RefreshTableList();
                 }
                 catch(Exception ex)
                 {
@@ -86,5 +93,130 @@ namespace Travel_Agent
             return string.Empty;
         }
         #endregion
+
+        private void button_RefreshTabList_Click(object sender, EventArgs e)
+        {
+            RefreshTableList();
+        }
+
+        private void RefreshTableList()
+        {
+            if(string.IsNullOrEmpty(this._DB_File_Name))
+            {
+                MessageBox.Show("請先選擇 Data Base 檔案!");
+                return;
+            }
+            using (OleDbConnection conn = OleDbOpenConn(this._DB_File_Name))
+            {
+                string[] restrains = new string[4];
+                restrains[3] = "Table";
+                DataTable table = conn.GetOleDbSchemaTable(OleDbSchemaGuid.Tables, restrains);
+                comboBox_TableList.DataSource = table;
+                comboBox_TableList.DisplayMember = "TABLE_NAME";
+            }
+        }
+
+        #region DB Connection
+        public static OleDbConnection OleDbOpenConn(string dbFilePath)
+        {
+            string cnstr = string.Format("Provider=Microsoft.ACE.OLEDB.12.0;Data Source=" + dbFilePath);
+            OleDbConnection icn = new OleDbConnection();
+            icn.ConnectionString = cnstr;
+            if (icn.State == ConnectionState.Open) icn.Close();
+            icn.Open();
+            return icn;
+        }
+        #endregion
+
+        private void comboBox_TableList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            DataRowView row = (DataRowView)this.comboBox_TableList.SelectedItem;
+            string table_name = row["TABLE_NAME"].ToString().Trim();
+            if(!string.IsNullOrEmpty(table_name))
+            {
+                using (OleDbConnection conn = OleDbOpenConn(this._DB_File_Name))
+                {
+                    OleDbCommand cmd = conn.CreateCommand();
+                    cmd.CommandText = string.Format("SELECT * FROM [{0}]", table_name);
+                    OleDbDataAdapter adapter = new OleDbDataAdapter(cmd);
+                    DataTable table = new DataTable();
+                    adapter.Fill(table);
+                    dataGridView_TravelDataList.DataSource = table;
+                }
+            }
+        }
+
+        private void button_AddSelection_Click(object sender, EventArgs e)
+        {
+            // to copy the rows you need to have created the columns:
+            foreach (DataGridViewColumn c in this.dataGridView_TravelDataList.Columns)
+            {
+                if(this.dataGridView_Result.Columns[c.HeaderText] == null)
+                {
+                    this.dataGridView_Result.Columns.Add(c.Clone() as DataGridViewColumn);
+                }
+            }
+
+            //then you can copy the rows values one by one (working on the selectedrows collection)
+            foreach (DataGridViewRow srcRow in this.dataGridView_TravelDataList.SelectedRows)
+            {
+                // initial a new row instance
+                int index = this.dataGridView_Result.Rows.Add(srcRow.Clone() as DataGridViewRow);
+                foreach(DataGridViewCell cell in srcRow.Cells)
+                {
+                    string colName = this.dataGridView_TravelDataList.Columns[cell.ColumnIndex].HeaderText;
+                    this.dataGridView_Result[colName, index].Value = cell.Value;
+                }
+
+            }
+        }
+
+        private void button_ExportResult_Click(object sender, EventArgs e)
+        {
+            // data grid view row count is greater than 0, user had added some data
+            if(this.dataGridView_Result.Rows.Count > 0)
+            {
+                SaveFileDialog sfdlg = new SaveFileDialog();
+                sfdlg.AddExtension = true;
+                sfdlg.DefaultExt = ".xlsx";
+                sfdlg.Filter = "Microsoft Excel File (*.xlsx)|";
+
+                if(sfdlg.ShowDialog() == DialogResult.OK)
+                {
+                    // conver DataGridView to DataTable first
+                    DataTable dt = new DataTable("Result");
+                    // generate DataTable columns
+                    foreach(DataGridViewColumn col in this.dataGridView_Result.Columns)
+                    {
+                        if(col.Visible && !string.IsNullOrEmpty(col.Name))
+                        {
+                            dt.Columns.Add(col.Name);
+                            dt.Columns[col.Name].Caption = col.HeaderText;
+                        }
+                    }
+                    // copy every rows in DataGridView
+                    foreach(DataGridViewRow row in this.dataGridView_Result.Rows)
+                    {
+                        // new a data table row first
+                        DataRow dtRow = dt.NewRow();
+                        // copy column values secondly
+                        foreach(DataColumn col in dt.Columns)
+                        {
+                            dtRow[col.ColumnName] = row.Cells[col.ColumnName].Value;
+                        }
+                        // add completely row back to DataTable
+                        dt.Rows.Add(dtRow);
+                    }
+
+                    // save data table to Excel worksheet
+                    using (XLWorkbook wb = new XLWorkbook())
+                    {
+                        wb.AddWorksheet(dt);
+                        wb.SaveAs(sfdlg.FileName);
+                        MessageBox.Show("儲存成功!");
+                    }
+                }
+            }
+        }
     }
 }
