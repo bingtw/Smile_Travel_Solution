278936aadc02367f095df2c344869fba2dfa77ea
Bing Hsueh <bing.tangbing@gmail.com>
2016-01-09 23:32:43 +0800

    update deploy commnd and pre-deploy script

--- pre_deploy.sh
+++ pre_deploy.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+set -v 
 echo "=== pre-deploy.sh start ==="
 pushd ${TRAVIS_BUILD_DIR}
 echo change working directory to TRAVIS_BUILD_DIR=${TRAVIS_BUILD_DIR}
 zip binaries.zip -r Travel_Agent/bin/Release
 
 echo "=== pre-deploy.sh end ==="
+
