0392f2751cfe03d0a760ee553ff5084941e30c51
Bing Hsueh <bing.tangbing@gmail.com>
2016-01-09 19:27:13 +0800

    update travis config for auto deploy

--- pre_deploy.sh
+++ pre_deploy.sh
@@ -1,3 +1,8 @@
 #!/bin/bash
 
+echo "=== pre-deploy.sh start ==="
+pushd ${TRAVIS_BUILD_DIR}
+echo change working directory to TRAVIS_BUILD_DIR=${TRAVIS_BUILD_DIR}
 zip binaries.zip -r Travel_Agent/bin/Release
+
+echo "=== pre-deploy.sh end ==="
